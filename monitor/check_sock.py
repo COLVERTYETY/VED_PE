71167f37793c9aada85f07239008eb0937251af3
nicolas <nicolas.jan.stas@gmail.com>
2022-06-22 18:17:52 +0200

    ididi

--- monitor/check_sock.py
+++ monitor/check_sock.py
@@ -5,7 +5,7 @@ import time
 # create tcp socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.settimeout(5) # for 5 sec
-sock.connect(('217.160.70.219', 8080))
+sock.connect((socket.gethostname(), 8080)) # '217.160.70.219'
 
 #sleep for 1 s
 time.sleep(1)
