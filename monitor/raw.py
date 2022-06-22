2cb60af407deefe05b705f34f34e56ca0b29853f
nicolas <nicolas.jan.stas@gmail.com>
2022-06-22 20:24:34 +0200

    ww

--- monitor/raw.py
+++ monitor/raw.py
@@ -1,50 +1,10 @@
-import socket
-import matplotlib.pyplot as plt
-import numpy as np
-
-
-datasize = 300
-
-Tmoteur = []
-Tbatterie = []
-Tmosfet = []
-
-Courant = []
-
-vitesse = []
-rpm = []
-
-Vbatterie = []
-Vmoteur = []
-
-puissance = []
-consigne = []
-dutycycle = [] 
-
-
-trick = 10
-
-
-# # Create a TCP/IP socket
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-# # print all data received on the socket
-sock.bind(("0.0.0.0", 8080))
-sock.listen(50)
-# try:
-while True:
-    print("start")
-    conn, addr = sock.accept()
-    print('Connected by', addr)
-    while True:
-        data = conn.recv(1024)
-        if not data:
-            break
-        trick +=1
-        # Print the contents of the serial data
-        raw = data.decode('utf-8')
-        print(raw)
-
-# except Exception as e:
-#     print("KeyboardInterrupt", e)
-#     sock.close()
+import asyncio
+import websockets
+# create handler for each connection
+async def handler(websocket, path):
+    data = await websocket.recv()
+    reply = f"Data recieved as:  {data}!"
+    await websocket.send(reply)
+start_server = websockets.serve(handler, "0.0.0.0", 8080)
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever()
