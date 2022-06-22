d34a10feb4541b79d1a5950334d83103e8a7abe5
nicolas <nicolas.jan.stas@gmail.com>
2022-06-22 22:53:03 +0200

    changed socket

--- monitor/raw.py
+++ monitor/raw.py
@@ -1,10 +1,19 @@
-import asyncio
-import websockets
-# create handler for each connection
-async def handler(websocket, path):
-    data = await websocket.recv()
-    reply = f"Data recieved as:  {data}!"
-    await websocket.send(reply)
-start_server = websockets.serve(handler, "0.0.0.0", 8080)
-asyncio.get_event_loop().run_until_complete(start_server)
-asyncio.get_event_loop().run_forever()
+"""
+UDP echo server that converts a message
+received from client into uppercase and
+then sends it back to client. 
+"""
+from socket import *
+# Port number of server
+server_port = 8080
+# Server using IPv4 and UDP socket
+server_socket = socket(AF_INET, SOCK_DGRAM)
+# Bind server to port number and IP address
+server_socket.bind(("0.0.0.0", server_port))
+print("The server is ready to receive msg...")
+while True:
+    # Extract message and client address from received msg
+    message, client_address = server_socket.recvfrom(2048)
+    # Create response message
+    print(message)
+    # server_socket.sendto(modified_message, client_address)
