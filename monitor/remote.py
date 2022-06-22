f9aa11ac6d3a58a08871aa54b0baae0ae8238608
nicolas <nicolas.jan.stas@gmail.com>
2022-06-22 16:59:26 +0200

    full listen

--- monitor/remote.py
+++ monitor/remote.py
@@ -4,20 +4,14 @@ import socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # # print all data received on the socket
-# sock.connect(('127.0.0.1', 8080))
-
-# # print all data received on the socket
-# while True:
-#     data = sock.recv(1024)
-#     if not data:
-#         break
-#     print(data.decode('utf-8'))
-
-# sock.close()
-
-# accept a connection from an outside client
 sock.bind(('localhost', 8080))
-sock.listen(1)
-conn, addr = sock.accept()
-print('Connected by', addr)
+while True:
+    sock.listen(1)
+    conn, addr = sock.accept()
+    print('Connected by', addr)
+    while True:
+        data = conn.recv(1024)
+        if not data:
+            break
+        print(data.decode('utf-8'))
 
