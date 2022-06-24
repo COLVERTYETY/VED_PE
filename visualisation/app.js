727be1615201ccdab93cacca65de5d51e73b895c
nicolas <nicolas.jan.stas@gmail.com>
2022-06-24 13:29:12 +0200

    cool stuff

--- visualisation/app.js
+++ visualisation/app.js
@@ -1,36 +1,140 @@
 #!/usr/bin/env node
+var net = require('net');
+var HOST = '0.0.0.0';
+var PORT = 8080;
 
-const express = require('express')
-const bodyParser = require('body-parser')
-const app = express()
+var Chart = require('chart.js');
 
-// ajout de socket.io
-const server = require('http').Server(app)
-const io = require('socket.io')(server)
+const express = require('express');
+const path = require('path');
+const app = express();
+// const port = process.env.PORT || 8080;
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended:true}))
-app.use(express.static('public'))
-app.get('/', function (req, res) {
-   res.sendFile('index.html', { root: __dirname })
-})
+// sendFile will go here
+app.get('/graph', function(req, res) {
+    // get the canvas
 
-app.get('/json', function (req, res) {
-   res.status(200).json({"message":"ok"})
-})
-
-// établissement de la connexion
-io.on('connection', (socket) =>{
-   console.log(`Connecté au client ${socket.id}`)
-})
-
-// display data received on socket
-io.on('data', (data) => {
-    console.log(data)
-})
+    // res.sendFile(path.join(__dirname, '/index.html'));
+});
+app.listen(9999);
+console.log('Server started at http://localhost:' + 9999);
 
 
-// on change app par server
-server.listen(8080, function () {
- console.log('Votre app est disponible sur localhost:3000 !')
-})
+// params 
+
+maxsize = 100;
+
+// arrays to hold the data
+var Tmoteur = []
+var Tbatterie = []
+var Tmosfet = []
+
+var Courant = []
+
+var vitesse = []
+var rpm = []
+
+var Vbatterie = []
+var Vmoteur = []
+
+var puissance = []
+var consigne = []
+var dutycycle = [] 
+
+net.createServer(function(sock) {
+    console.log('CONNECTED:',sock.remoteAddress,':',sock.remotePort);
+    sock.setEncoding("utf8"); //set data encoding (either 'ascii', 'utf8', or 'base64')
+    sock.on('data', function(data) {
+        // console.log(data);
+        try {
+            const parsed_data = JSON.parse(data)
+            console.log(parsed_data)
+            // check if "A" is a key in the parsed data
+            if ("A" in parsed_data) {
+                // add teh value to the array
+                Courant.push(parseFloat(parsed_data["A"]))
+            }
+            if ("M" in parsed_data) {
+                Tmoteur.push(parseFloat(parsed_data["M"]))
+            }
+            if ("B" in parsed_data) {
+                Tbatterie.push(parseFloat(parsed_data["B"]))
+            }
+            if ("0" in parsed_data) {
+                Tmosfet.push(parseFloat(parsed_data["0"]))
+            }
+            if ("U" in parsed_data) {
+                Vbatterie.push(parseFloat(parsed_data["U"]))
+            }
+            if ("T" in parsed_data) {
+                Vmoteur.push(parseFloat(parsed_data["T"]))
+            }
+            if ("R" in parsed_data) {
+                rpm.push(parseFloat(parsed_data["R"]))
+            }
+            if ("V" in parsed_data) {
+                vitesse.push(parseFloat(parsed_data["V"]))
+            }
+            if ("D" in parsed_data) {
+                dutycycle.push(parseFloat(parsed_data["D"]))
+            }
+            if ("P" in parsed_data) {
+                puissance.push(parseFloat(parsed_data["P"]))
+            }
+            if ("C" in parsed_data) {
+                consigne.push(parseFloat(parsed_data["C"]))
+            }
+          } catch(err) {
+            console.error(err)
+          }
+          // check if data is too long
+            if (Tmoteur.length > maxsize) {
+                // remove the first element
+                Tmoteur.shift()
+            }
+            if (Tbatterie.length > maxsize) {
+                // remove the first element
+                Tbatterie.shift()
+            }
+            if (Tmosfet.length > maxsize) {
+                // remove the first element
+                Tmosfet.shift()
+            }
+            if (Courant.length > maxsize) {
+                // remove the first element
+                Courant.shift()
+            }
+            if (Vbatterie.length > maxsize) {
+                // remove the first element
+                Vbatterie.shift()
+            }
+            if (Vmoteur.length > maxsize) {
+                // remove the first element
+                Vmoteur.shift()
+            }
+            if (rpm.length > maxsize) {
+                // remove the first element
+                rpm.shift()
+            }
+            if (vitesse.length > maxsize) {
+                // remove the first element
+                vitesse.shift()
+            }
+            if (dutycycle.length > maxsize) {
+                // remove the first element
+                dutycycle.shift()
+            }
+            if (puissance.length > maxsize) {
+                // remove the first element
+                puissance.shift()
+            }
+            if (consigne.length > maxsize) {
+                // remove the first element
+                consigne.shift()
+            }
+
+    });
+
+}).listen(PORT, HOST, function() {
+    console.log("server accepting connections");
+});
---
+++ visualisation/test.js
@@ -0,0 +1,87 @@
+#!/usr/bin/env node
+
+var net = require('net');
+var HOST = '0.0.0.0';
+var PORT = 8080;
+
+// the usual
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// socket io
+const http = require('http');
+const server = http.createServer(app);
+const {Server} = require('socket.io');
+const io = new Server(server);
+
+
+// sendFile will go here
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, '/index.html'));
+});
+
+// handle static assets
+app.use(express.static(__dirname));
+
+// connect socket
+io.on('connection', function(socket) {
+    console.log('a user connected');
+});
+
+// we start the server last so that we can use the socket
+server.listen(9999, () => {
+    console.log('Server started at http://localhost:' + 9999);
+})
+
+net.createServer(function(sock) {
+    console.log('CONNECTED DATA:',sock.remoteAddress,':',sock.remotePort);
+    sock.setEncoding("utf8"); //set data encoding (either 'ascii', 'utf8', or 'base64')
+    sock.on('data', function(data) {
+        // console.log(data);
+        try {
+            const parsed_data = JSON.parse(data)
+            // console.log(parsed_data)
+            // check if "A" is a key in the parsed data
+            if ("A" in parsed_data) {
+                //send the value to all clients
+                io.emit('A', parseFloat(parsed_data["A"]));
+            }
+            if ("M" in parsed_data) {
+                io.emit('M', parseFloat(parsed_data["M"]));
+            }
+            if ("B" in parsed_data) {
+                io.emit('B', parseFloat(parsed_data["B"]));
+            }
+            if ("O" in parsed_data) {
+                io.emit('O', parseFloat(parsed_data["O"]));
+            }
+            if ("U" in parsed_data) {
+                io.emit('U', parseFloat(parsed_data["U"]));
+            }
+            if ("T" in parsed_data) {
+                io.emit('T', parseFloat(parsed_data["T"]));
+            }
+            if ("R" in parsed_data) {
+                io.emit('R', parseFloat(parsed_data["R"]));
+            }
+            if ("V" in parsed_data) {
+                io.emit('V', parseFloat(parsed_data["V"]));
+            }
+            if ("D" in parsed_data) {
+                io.emit('D', parseFloat(parsed_data["D"]));
+            }
+            if ("P" in parsed_data) {
+                io.emit('P', parseFloat(parsed_data["P"]));
+            }
+            if ("C" in parsed_data) {
+                io.emit('C', parseFloat(parsed_data["C"]));
+            }
+          } catch(err) {
+            console.error(err)
+            }
+    });
+
+}).listen(PORT, HOST, function() {
+    console.log("server accepting connections from device");
+});
